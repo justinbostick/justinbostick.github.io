af3995dbfdbe03a7b37d351f8b170f822b9fe0b9
justinbostick <bostijus000@warren.k12.in.us>
2017-11-21 09:56:50 -0500

    Add files via upload

---
+++ tribute/code.js
@@ -0,0 +1,17 @@
+$(function() {
+                $("#bg-info").click(function() {
+                    $(".test").toggle();
+                });
+})
+           $(function() {
+                $("#accolades").click(function() {
+                    $(".accolades").toggle(1000);
+                });
+            })
+            
+             $(function() {
+                $("#contribution").click(function() {
+                    $(".contribution").toggle(1000);
+                });
+            })
+            
